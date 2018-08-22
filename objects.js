011adbbe4d91872843c9573fdfabc3fab6a1941f
Annie Eby <eby.annie@gmail.com>
2018-08-22 19:59:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,8 @@
 var playlist = new Object({ artistName: 'songTitle' })
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  /*playlist['artistName'] = songTitle
-  playlist['artistName'] = songTitle  /*line 146 of readme*/
 
-  Object.assing({}, playlist,{ [artistName]: songTitle})}
-  /*
-  Object.assign({ artistName: 'songTitle' }) /*line 192 of readme -- We can use `Object.assign()` to create a new object and pass it properties from existing objects.*/
-  */
+  Object.assign({}, playlist,{ [artistName]: songTitle})}
   return playlist
 }
 
