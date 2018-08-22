1c96ed60d195fd75cd863f28e270ba88f6e32882
Annie Eby <eby.annie@gmail.com>
2018-08-22 20:29:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,10 @@
 var playlist = new Object ({ artistName: 'songTitle' })
 
-function updatePlaylist(playlist, artistName, songTitle)
+function updatePlaylist(playlist, artistName, songTitle) {
 playlist['artistName'] = 'songTitle'
 return playlist
-
+}
+/*
 function updatePlaylist(playlist, artistName, songTitle) {
 return
   Object.assign({}, playlist,{ [artistName]: songTitle})
