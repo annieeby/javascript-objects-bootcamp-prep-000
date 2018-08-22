9264bca917575d5442166fdcee24f0568dbe6434
Annie Eby <eby.annie@gmail.com>
2018-08-22 20:32:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = new Object ({ artistName: 'songTitle' })
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return
-    playlist['artistName'] = 'songTitle'
+    playlist[artistName] = songTitle
 }
 /*
 function updatePlaylist(playlist, artistName, songTitle) {
